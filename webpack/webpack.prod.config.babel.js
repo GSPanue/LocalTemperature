df7aa0d1b3a41d7af13723527721bce07a8e0b3d
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-14 22:42:50 +0000

    Added development and production webpack configs

---
+++ webpack/webpack.prod.config.babel.js
@@ -0,0 +1,41 @@
+import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import path from 'path';
+
+const root = path.resolve(__dirname, '..');
+
+module.exports = {
+  mode: 'production',
+  devtool: '',
+  entry: path.join(root, 'src'),
+  output: {
+    path: path.join(root, 'dist/js'),
+    publicPath: '/js/',
+    filename: 'bundle.min.js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: [
+        path.join(root, 'node_modules')
+      ],
+      loader: [
+        'babel-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      cache: true,
+      parallel: true,
+      sourceMap: true
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disable',
+      generateStatsFile: true,
+      statsOptions: {
+        source: false
+      }
+    })
+  ]
+};
