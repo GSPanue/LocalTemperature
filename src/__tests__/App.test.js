21d79ab7cac63567fde09cba8b4d550699ea0588
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-16 00:25:12 +0000

    Added entry point

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../App';
+
+describe('Component: App', () => {
+  it('should render without crashing', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper).toHaveLength(1);
+  });
+});
