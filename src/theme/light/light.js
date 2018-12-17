5b7d06fe6a44278f0cbff6f12d51a9c802c980cc
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-17 15:45:37 +0000

    Added theme styles

--- src/theme/light/light.js
+++ src/theme/light/light.js
@@ -1,3 +1,28 @@
-const light = {};
+/**
+ * Color palette
+ */
+const palette = {
+  mirage: '#171c24',
+  fiord: '#3c4b62',
+  dodgerBlue: '#3880ff',
+  white: '#ffffff'
+};
+
+/**
+ * Light theme
+ */
+const light = {
+  text: {
+    primaryColor: palette.mirage,
+    secondaryColor: palette.fiord,
+    small: '10px',
+    medium: '14px',
+    large: '18px'
+  },
+  spinner: {
+    color: palette.mirage,
+    thickness: '4'
+  }
+};
 
 export default light;
