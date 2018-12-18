67dec6b8e9c9273a9713c66d31125ca086110a52
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-18 10:28:36 +0000

    Added geolocation utils

---
+++ src/utils/geolocation/geolocation.js
@@ -0,0 +1,29 @@
+/**
+ * getCurrentPosition: Returns the current position of the device.
+ */
+const getCurrentPosition = () => {
+  const { geolocation } = navigator;
+
+  return (new Promise((resolve, reject) => {
+    if (geolocation) {
+      // Geolocation is supported
+      // Return current position
+      return (
+        geolocation.getCurrentPosition(resolve, reject)
+      );
+    }
+
+    // Geolocation isn't supported
+    // Return rejected promise
+    return (
+      reject(new Error('Your browser is not supported'))
+    );
+  }));
+};
+
+const x = () => (2);
+
+export {
+  getCurrentPosition,
+  x
+};
