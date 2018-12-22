eedf5c99e48e1accaace2beba5c6e03c7cc283c5
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-22 19:59:37 +0000

    Refactored Error component

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -4,13 +4,12 @@ import PropTypes from 'prop-types';
 import { Wrapper } from './styles';
 
 const propTypes = {
-  error: PropTypes.string.isRequired
+  error: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired
 };
 
-const Error = ({ error }) => (
-  <Wrapper>
-    {error}
-  </Wrapper>
+const Error = ({ error, children }) => (
+  (error) ? <Wrapper>{error}</Wrapper> : children
 );
 
 Error.propTypes = propTypes;
--- src/components/Error/__tests__/Error.test.js
+++ src/components/Error/__tests__/Error.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
+import { mountWithTheme } from '../../../utils';
+
 import Error from '..';
 import { Wrapper } from '../styles';
 
 describe('Component: Error', () => {
   const minProps = {
-    error: 'error'
+    error: 'error',
+    children: <div />
   };
 
   it('should render without crashing', () => {
@@ -15,22 +18,53 @@ describe('Component: Error', () => {
     expect(wrapper).toHaveLength(1);
   });
 
-  it('should render a Wrapper component', () => {
+  it('should render a Wrapper component when error is not empty', () => {
     const wrapper = shallow(<Error {...minProps} />);
 
     expect(wrapper.find(Wrapper)).toHaveLength(1);
   });
 
-  it('should render error as text', () => {
+  it('should render error as text when error is not empty', () => {
     const wrapper = shallow(<Error {...minProps} />);
 
     expect(wrapper.text()).toEqual('error');
   });
 
-  it('should have props for message', () => {
+  it('should not render children when error is not empty', () => {
     const wrapper = shallow(<Error {...minProps} />);
 
-    expect(wrapper.props().children).toBeDefined();
-    expect(wrapper.props().children).toEqual('error');
+    expect(wrapper.find('div')).toHaveLength(0);
+  });
+
+  it('should render children when error is empty', () => {
+    const wrapper = shallow(<Error {...minProps} error="" />);
+
+    expect(wrapper.find('div')).toHaveLength(1);
+  });
+
+  it('should not render a Wrapper component when error is empty', () => {
+    const wrapper = shallow(<Error {...minProps} error="" />);
+
+    expect(wrapper.find(Wrapper)).toHaveLength(0);
+  });
+
+  it('should not render error as text when error is empty', () => {
+    const wrapper = shallow(<Error {...minProps} error="" />);
+
+    expect(wrapper.text()).toEqual('');
+  });
+
+  it('should have props for error and children', () => {
+    const theme = {
+      text: {
+        large: '18px',
+        bold: 'bold'
+      }
+    };
+
+    const wrapper = mountWithTheme(<Error {...minProps} />, theme);
+
+    expect(wrapper.find('Error').props().error).toBeDefined();
+    expect(wrapper.find('Error').props().children).toBeDefined();
   });
 });
