619fbc2042b2fdbf017e892c8d3a62676b6f892e
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-27 14:02:25 +0000

    Increased padding

--- src/components/Card/__tests__/styles.test.js
+++ src/components/Card/__tests__/styles.test.js
@@ -15,7 +15,7 @@ describe('Styles: Card', () => {
     it('should have the correct styles', () => {
       const wrapper = mount(<Wrapper theme={theme} />);
 
-      expect(wrapper).toHaveStyleRule('padding', '50px');
+      expect(wrapper).toHaveStyleRule('padding', '80px');
       expect(wrapper).toHaveStyleRule('border-radius', '6px');
       expect(wrapper).toHaveStyleRule('box-shadow', '0 3px 6px 0 rgba(0,0,0,.1),0 1px 3px 0 rgba(0,0,0,.08)');
       expect(wrapper).toHaveStyleRule('background', theme.card.primaryBackground);
